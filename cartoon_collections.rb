b3f589acd4fdf5958dcd2f765394fff1d7163f2a
Alexander Schelchere <ascellie@gmail.com>
2019-05-12 18:36:31 -0400

    find the cheese - complete

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,4 +20,7 @@ end
 def find_the_cheese(cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.find do |cheese|
+    cheese_types.include?(cheese) ? true : false
+  end
 end
